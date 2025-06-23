26e635434e0dcd6a9b04e83f046c742a4af674cc
Mehra ayisha <mehratacharakkal@gmail.com>
2025-06-23 10:36:47 +0530

    cd added

--- main.c
+++ main.c
@@ -7,8 +7,34 @@
 #define MAX_ARGS 64
 void prompt()
 {
-  printf("tinysh>>");
+  printf("tinysh>> ");
   fflush(stdout);
+}
+void change_dir(char **args)
+{
+   char *target_dir=args[1];
+   if(target_dir == NULL)
+   {
+    printf("enter a target directory\n");
+    return;
+   }
+   else if(strcmp(target_dir,"-")==0)
+  {
+   //getenv get the value of OLDPWD
+   target_dir=getenv("OLDPWD");
+     if(target_dir==NULL)
+     {
+       fprintf(stderr,"cd:No previous directory\n");
+       return;
+     }
+     printf("%s\n",target_dir);
+  }
+  //chdir return 0 if success else -1
+  if(chdir(target_dir)!= 0)
+   {
+    perror("cd");
+    return;
+   }
 }
 void execute_cmd(char *input)
 {
@@ -27,12 +53,19 @@ void execute_cmd(char *input)
 
   if(strcmp(args[0],"exit")==0)
   exit(0);
-
+  
+  if(strcmp(args[0],"cd")==0)
+  {
+   change_dir(args);
+   return;
+   
+  }
+  
   pid_t pid=fork();
   if(pid == 0)
   {
    execvp(args[0],args);
-   perror("command failed");
+   perror("command failed\n");
    exit(1);
   }
   else if(pid>0)
@@ -41,12 +74,12 @@ void execute_cmd(char *input)
   }
   else
   {
-   perror("fork failed");
+   perror("fork failed\n");
   }
 
 }
 int main()
-{
+ {
   char input[MAX_LINE];
   while(1)
   {
