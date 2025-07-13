a3ac82882e6fda30420ed43dd46b4dd5b3f3efae
Mehra ayisha <mehratacharakkal@gmail.com>
2025-07-13 22:46:18 +0530

    weather python2

--- plugins/weather.py
+++ plugins/weather.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env/python3
 import os
-import request
-def getlocation():
+import requests
+def get_location():
    try:
-	r=request.get("https://ipinfo.io/json")#ipinfo.io is public ip geolocation api whuch fetch the location and ip in a json string 
-	data=r.json()#convert to python dict
-	return data["city"],data["region"]
+     r=requests.get("https://ipinfo.io/json")
+     #ipinfo is a public geolocaation api to get location info
+     #json() covert json string to python dict
+     data=r.json()#convert to python dict
+     #print(data)
+     return data["city"],data["region"]
    except:
-   	  return "Unknown","Unknown"
-
+     return "Unknown","Unknown"
+get_location()
 def get_weather(city):
-	API_KEY=os.getenv("OWN_API_KEY")#os is a python module that allow interaction with operating systems
-    if not API_KEY:
-    	return :"API KEY isn't set in .env"
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-    r=request.get(url)#send request to weather api
-    data=r.json()
-    
+   API_KEY=os.getenv("OWN_API_KEY")
+   #os is a python module that allow interaction with operating systems
+   if not API_KEY:
+     return :"API KEY isn't set in .env"
+   url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+   try:
+     r=requests.get(url)
+     #send request to weather api
+     data=r.json()
+     temp=data["main"]['temp']
+     desc=data['weather'][0]['description'].capitalize()
+     icon=data['weather'][0]['icon']
+     emoji = {
+            "01": "☀️", "02": "🌤️", "03": "⛅", "04": "☁️","09": "🌧️", "10": "🌦️", "11": "⛈️", "13": "❄️", "50": "🌫️"}.get(icon[:2], "🌡️")
+     return f"{emoji} {temp}°C, {desc}"
+   except:
+     return "could not get weather"
+
